fde5b2c4afc97afb27dd9f42bf6ddc02ce45580a
Michał Cesarczyk <michal.cesarczyk@gmail.com>
2023-07-08 11:20:56 +0200

    feat(keycloak): install and initialize
    1. add deps, 2. create class components to run POC, 3. temporarily disable strict mode to evoid
    reload infinite loop

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { BrowserRouter, Route, Link, Routes } from "react-router-dom";
+import Welcome from "./Welcome";
+import Secured from "./Secured";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="container">
+          <ul>
+            <li>
+              <Link to="/">public component</Link>
+            </li>
+            <li>
+              <Link to="/secured">secured component</Link>
+            </li>
+          </ul>
+          <Routes>
+            <Route path="/" element={<Welcome />} />
+            <Route path="/secured" element={<Secured />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
-
 export default App;
---
+++ src/Secured.tsx
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import Keycloak from "keycloak-js";
+
+interface SecuredState {
+  keycloak: any;
+  authenticated: boolean;
+}
+
+interface SecuredProps {}
+
+class Secured extends Component<SecuredProps, SecuredState> {
+  constructor(props: SecuredProps) {
+    super(props);
+    this.state = { keycloak: null, authenticated: false };
+  }
+
+  componentDidMount() {
+    const keycloak = new Keycloak("/keycloak.json");
+    keycloak.init({ onLoad: "login-required" }).then((authenticated) => {
+      this.setState({ keycloak: keycloak, authenticated: authenticated });
+    });
+  }
+
+  render() {
+    if (this.state.keycloak) {
+      if (this.state.authenticated)
+        return (
+          <div>
+            <p>
+              This is a Keycloak-secured component of your application. You
+              shouldn't be able to see this unless you've authenticated with
+              Keycloak.
+            </p>
+          </div>
+        );
+      else return <div>Unable to authenticate!</div>;
+    }
+    return <div>Initializing Keycloak...</div>;
+  }
+}
+export default Secured;
